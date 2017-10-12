75ca890ea9f688ca6487c8a84837bbcf37e84393
elari01 <Rijin.Elayambari@ca.com>
2017-10-12 22:11:53 +0530

    Buttons and Image View added

--- src/js/app.js
+++ src/js/app.js
@@ -4,29 +4,49 @@ let images = ['lorempixel'];
 let Gallery = function(images, target) {
     this.container = target ? document.getElementById(target) : document.getElementsByTagName('body')[0];
     this.images = images;
+    this.currentImageIndex = 0;
 
     this.init = function(){
         let self = this;
         //self.container.
-        self.createButtons();
-        
-
+        //self.createButtons();
+        self.createView();
 
     }
 
-    this.createButtons = function(){
-        let self = this;
+    this.createButtonsView = function(){
+        let buttonContainer = document.createElement('div');
         let nextBtn = document.createElement('button');
         let prevBtn = document.createElement('button');
+        nextBtn.className = 'next-btn';
+        nextBtn.innerHTML = 'Next >';
+        prevBtn.className = 'prev-btn';
+        prevBtn.innerHTML = '< Previous';
 
         //Events
         nextBtn.addEventListener("click", self.nextImage);
         prevBtn.addEventListener("click", self.prevImage);
+        buttonContainer.appendChild(prevBtn);
+        buttonContainer.appendChild(nextBtn);
+        return buttonContainer;
+    }
 
-        self.container.appendChild(prevBtn);
-        self.container.appendChild(nextBtn);
-        
+    this.createImageView = function() {
+        let imageContainer = document.createElement('div');
+        let imgTag = document.createElement('img');
+        imgTag.className = 'image-container'; 
+        imageContainer.appendChild(imgTag);
 
+        return imageContainer;
+    }
+
+    this.createView = function(){
+        let self = this;
+        let galleryContainer = document.createElement('div');
+        galleryContainer.appendChild( self.createImageView() );
+        galleryContainer.appendChild( self.createButtonsView() );
+
+        self.container.appendChild(galleryContainer);
     }
 
     this.getImages = function(){
